756dc4d439c44fafb9a1f55c522f45b40a656945
Wojciech Fiołka <fiolkawojciech@gmail.com>
2021-12-15 10:45:57 +0100

    Add route for results.html

--- web/web_page.py
+++ web/web_page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__, template_folder='templates')
 
@@ -6,6 +6,14 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/home', methods=['GET', 'POST'])
 @app.route('/start', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        url = request.form.get('url_input')
+        return redirect(url_for('results', url=url))
     return render_template('index.html')
+
+@app.route('/results/<path:url>')
+def results(url):
+    return render_template('results.html', url=url)
+
 
 app.run()
