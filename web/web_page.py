736bcabd3bc63a6b2f19f8d0a5d8c5764bea855d
Wojciech Fiołka <fiolkawojciech@gmail.com>
2021-12-21 17:21:11 +0100

    Add route for thanks, adding email to the email list database

--- web/web_page.py
+++ web/web_page.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for
 
 import checker
+from checker import is_rick_roll
 
 app = Flask(__name__, template_folder='templates')
 
@@ -17,9 +18,23 @@ def index():
 @app.route('/results/<path:url>', methods=['GET', 'POST'])
 def results(url):
     if request.method == 'POST':
-        return redirect(url_for('index'))
+        if request.form.get('back') == 'Check another link!':
+            return redirect(url_for('index'))
+        if request.form.get('email1') is not None:
+            db = checker.db
+            new_email = request.form.get('email')
+            emails = db.collection('users').document('emails').get().to_dict()
+            last_id = int(list(emails.keys())[-1])
+            emails[str(last_id+1)] = new_email
+            db.collection('users').document('emails').set(emails)
+            return redirect(url_for('thanks'))
     return render_template('results.html',
-                           is_rick_roll=checker.is_rick_roll(url))
+                           is_rick_roll=is_rick_roll(url))
 
+@app.route('/thanks', methods=['GET', 'POST'])
+def thanks():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('thanks.html')
 
 app.run()
