bd791a6694a9aab59011637ad1b6ed56fd2ba851
Wojciech Fiołka <fiolkawojciech@gmail.com>
2021-12-21 17:11:31 +0100

    checking is link in our database for rickroll links

--- checker.py
+++ checker.py
@@ -1,2 +1,18 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("../serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 def is_rick_roll(url):
-    pass
+    if is_in_db(url):
+        return True
+
+def is_in_db(user_url):
+    urls = db.collection('rickroll').document('urls').get().to_dict()
+    for url in urls:
+        if url in user_url:
+            return True
+    return
